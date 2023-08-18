dfe6747d0116034e361f139e378d772721461c64
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-08-18 12:33:52 -0400

    instructor-names.js kata

---
+++ instructor-names.js
@@ -0,0 +1,29 @@
+const instructorWithLongestName = function (instructors) {
+  // Set an object with the same key-values to compare against
+  let longName = {name: "", course: ""};
+
+  // check each instructor's name length against the placeholder
+  for (const inst of instructors){
+    if (inst.name.length > longName.name.length){
+      longName = inst;
+    }
+  }
+
+  return longName;
+};
+
+console.log(
+  instructorWithLongestName([
+    { name: "Samuel", course: "iOS" },
+    { name: "Jeremiah", course: "Web" },
+    { name: "Ophilia", course: "Web" },
+    { name: "Donald", course: "Web" },
+  ])
+);
+console.log(
+  instructorWithLongestName([
+    { name: "Matthew", course: "Web" },
+    { name: "David", course: "iOS" },
+    { name: "Domascus", course: "Web" },
+  ])
+);
