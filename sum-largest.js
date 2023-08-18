f8bba1e095db21d11e41659875be61888a747e08
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-08-18 12:03:39 -0400

    completed sum-largest.js kata

---
+++ sum-largest.js
@@ -0,0 +1,21 @@
+const sumLargestNumbers = function(data) {
+  let firstNum = 0;
+  let secondNum = 0;
+
+  for (let i = 0; i < data.length; i++){
+    /*
+      if the current index is greater than firstNum,
+        set secondNum to firstNum, 
+        then set firstNum to current index
+    */ 
+    if (data[i] > firstNum){
+      secondNum = firstNum;
+      firstNum = data[i];
+    }
+  }
+  return firstNum + secondNum;
+}
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
