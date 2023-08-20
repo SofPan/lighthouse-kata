11742cb01596e5d0c2c2ec7e2fa434cf7bfc4806
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-08-20 19:51:25 -0400

    camelCase.js stretch kata

---
+++ camelCase.js
@@ -0,0 +1,12 @@
+const camelCase = function (input) {
+  const words = input.split(' ');
+  for (let i = 1; i < words.length; i++){
+    words[i] = words[i].replace(words[i][0], words[i][0].toUpperCase());
+  }
+
+  return words.join('');
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
