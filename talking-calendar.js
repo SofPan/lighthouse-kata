20e1e4747086c71ee01e61f70b040f4f201ed301
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-08-21 12:23:19 -0400

    talking-calendar.js

---
+++ talking-calendar.js
@@ -0,0 +1,28 @@
+const talkingCalendar = function (date) {
+  // use a deconstructor to define the split dates
+  let [year, month, day] = date.split('/');
+
+  const calendarMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  // set the month to the calendar month - 1 for correct index
+  month = calendarMonths[month - 1];
+
+  // depending on the day's last digit, append the correct suffix
+  if (day.charAt(1) === "2"){
+    day = day + 'nd';
+  } else if (day.charAt(1) === "3"){
+    day = day + 'rd';
+  } else {
+    day = day + 'th';
+  }
+
+  // If the day starts with 0, drop it
+  if (day.charAt(0) === "0"){
+    day = day.replace("0", "");
+  }
+
+  return month + " " + day + ", " + year;
+};
+
+console.log(talkingCalendar("2017/12/02"));
+console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("1987/08/24"));
