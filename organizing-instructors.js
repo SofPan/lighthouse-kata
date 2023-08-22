9d970b036e8ef660a6ca100e53d75827d40f06af
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-08-22 14:56:19 -0400

    organizing-instructors.js

---
+++ organizing-instructors.js
@@ -0,0 +1,31 @@
+const organizeInstructors = function (instructors) {
+  let output = {};
+
+  for (const member of instructors){
+    // If the output object does not include the course already, create an array
+    if (!output[member.course]){
+      output[member.course] = [];
+    }
+    // push the instructor's name to the relevant course in output
+    output[member.course].push(member.name);
+  }
+
+  return output;
+};
+
+console.log(
+  organizeInstructors([
+    { name: "Samuel", course: "iOS" },
+    { name: "Victoria", course: "Web" },
+    { name: "Karim", course: "Web" },
+    { name: "Donald", course: "Web" },
+  ])
+);
+console.log(
+  organizeInstructors([
+    { name: "Brendan", course: "Blockchain" },
+    { name: "David", course: "Web" },
+    { name: "Martha", course: "iOS" },
+    { name: "Carlos", course: "Web" },
+  ])
+);
