f6a7ad079f89df7c800426daed08521f55336216
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-03 18:38:00 -0400

    adding comments

--- stock-market/stock.js
+++ stock-market/stock.js
@@ -3,10 +3,7 @@
   return the maximum profit made by buying a given price,
   then selling later on
 */
-/**
- * @function maxprofit takes in an array of prices (numbers)
- * @returns maximum profit or -1 if no profit is possible
- */
+// Check if argument is an array
 const checkIfIsArray = (arr, name) => {
   if (!Array.isArray(arr)) {
     throw new Error(`${name} requires an array`);
@@ -14,6 +11,7 @@ const checkIfIsArray = (arr, name) => {
   return true;
 };
 
+// sort ascending
 const sortPrices = (arr) => {
   checkIfIsArray(arr, "sortPrices");
   return arr.sort((a, b) => {
@@ -21,15 +19,21 @@ const sortPrices = (arr) => {
   });
 };
 
+// subtract two digits
 const findDiff = (n1, n2) => {
-  return n1 - n2;
+  return n2 - n1;
 };
 
+/**
+ * @function maxProfit takes in an array of prices (numbers)
+ * @param prices an array of prices
+ * @returns maximum profit or -1 if no profit is possible
+ */
 const maxProfit = (prices) => {
   checkIfIsArray(prices, "maxProfit");
   let max = -1;
   for (let i = 0; i < prices.length; i++) {
-    // sort from the next index to the end (to the right)
+    // sort from the next index to the end of the array
     const sortRight = sortPrices(prices.slice(i + 1));
     // subtract the current number from the largest available
     const diff = findDiff(sortRight[sortRight.length - 1], prices[i]);
@@ -41,4 +45,4 @@ const maxProfit = (prices) => {
   return max;
 };
 console.log(maxProfit([45, 24, 35, 31, 40, 38, 11]));
-module.exports = { maxProfit, checkIfIsArray, sortPrices, findDiff };
+module.exports = { maxProfit, checkIfIsArray, sortPrices };
