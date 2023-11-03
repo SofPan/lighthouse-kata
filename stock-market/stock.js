7bc3bd004450b49290e2b07dd6398c6a82d979cb
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-03 17:20:39 -0400

    set up stock-market kata

---
+++ stock-market/stock.js
@@ -0,0 +1,15 @@
+/*
+  Given a list of stock prices for a given day,
+  return the maximum profit made by buying a given price,
+  then selling later on
+*/
+/**
+ * @function maxprofit takes in an array of prices (numbers)
+ * @returns maximum profit or -1 if no profit is possible
+ */
+
+const maxprofit = (prices) => {
+  // code here
+};
+console.log(maxprofit([45, 24, 35, 31, 40, 38, 11]));
+module.exports = maxprofit;
