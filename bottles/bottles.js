a8cb1a24a50eb245d1f6e73d4d2ac2115350e838
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-04 23:15:15 -0400

    move countBottles to bottles object

--- bottles/bottles.js
+++ bottles/bottles.js
@@ -33,9 +33,8 @@ const bottles = {
   },
   totalBottles: function (purchased) {
     // Each bottle of pop costs $2
-    this.result.purchased = this.divideBy(purchased, 2);
-    const bottlesPurchased = this.result.purchased;
-    return this.result.maxTotalBottles += this.bottleCounter(bottlesPurchased, 0, 0);
+    // this.result.purchased = this.divideBy(purchased, 2);
+    let fullBottles = purchased;
   },
   divideBy: (n1, n2) => {
     return n1 / n2;
@@ -43,39 +42,15 @@ const bottles = {
   roundDown: (num) => {
     return Math.floor(num);
   },
-  bottleCounter: function (fullBottles, emptyBottles, bottleCaps) {
-    // if (fullBottles < 1 && emptyBottles < 2 && bottleCaps < 4) {
-    console.log(fullBottles, emptyBottles, bottleCaps);
-    if (emptyBottles < 2 && bottleCaps < 4 && fullBottles < 1) {
-      return "base case";
+  countBottles: function (recycledItem, perEach) {
+    let fullBottles = 0;
+    while (recycledItem > perEach) {
+      fullBottles += 1;
+      recycledItem -= perEach;
     }
-    let empties = emptyBottles + fullBottles;
-    let caps = 9;
-    let redeemEmpties = (empties - (empties % 2)) / 2;
-    let redeemCaps = caps % 4;
-
-    console.log(redeemEmpties, redeemCaps);
-    // let redeemedBottles = redeemEmpties + redeemCaps;
-
-    // empties = empties - redeemEmpties;
-    // caps = caps - redeemCaps;
-    // return redeemedBottles;
-    // return this.bottleCounter(redeemedBottles, empties, caps);
-  }
+    return [fullBottles, recycledItem];
+  },
 };
-
-const countBottles = (recycledItem, perEach) => {
-  let fullBottles = 0;
-  while (recycledItem > perEach) {
-    fullBottles += 1;
-    recycledItem -= perEach;
-  }
-  return [fullBottles, recycledItem];
-};
-
-let empties = 9;
-let each = 2;
-console.log(countBottles(empties, each));
 
 // console.log(bottles.totalBottles(10)); // 15
 // console.log(totalBottles(20)); // 35
