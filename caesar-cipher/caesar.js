aedaf14daecd2c169753d9a9f5610a058c808b05
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-09 11:36:14 -0500

    complete caesar cipher

--- caesar-cipher/caesar.js
+++ caesar-cipher/caesar.js
@@ -1,5 +1,31 @@
-const encrypt = (plaintext, key) => {
-
+const convertLetterToAscii = (char) => {
+  return char.charCodeAt(0);
 };
 
-module.exports = encrypt;
+const convertAsciiToLetter = (char) => {
+  return String.fromCharCode(char);
+};
+
+const encrypt = (plaintext, key) => {
+  let result = "";
+  for (let i = 0; i < plaintext.length; i++) {
+    if (plaintext[i] !== " ") {
+      let convertedChar = convertLetterToAscii(plaintext[i]) + key;
+      if (convertedChar < 97) {
+        let wrapNum = 97 - convertedChar - 1;
+        convertedChar = 122 - wrapNum;
+      }
+      if (convertedChar > 122) {
+        let wrapNum = convertedChar - 122 - 1;
+        convertedChar = 97 + wrapNum;
+      }
+      convertedChar = convertAsciiToLetter(convertedChar);
+      result += convertedChar;
+    } else {
+      result += " ";
+    }
+  }
+  return result;
+};
+
+module.exports = { encrypt };
